2750d5847a4f9e5aed8a226fd2265f772018de3f
Yassine OULED EL BAKRI <youledelbakri@PC-3015.gestform.fonctionnel>
2024-09-02 11:43:50 +0200

    Setup CI/CD

--- exec.sh
+++ exec.sh
@@ -5,25 +5,25 @@ set -e
 
 # setup kong
 docker-compose build kong
-sleep 2
+sleep 5
 docker-compose up -d kong-db
-sleep 2
+sleep 5
 docker-compose run --rm kong kong migrations bootstrap
-sleep 2
+sleep 5
 docker-compose run --rm kong kong migrations up
-sleep 2
+sleep 5
 docker-compose up -d kong
 docker-compose ps
 
 # Test liveness for Kong
-curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
+curl -v http://localhost:8001 | jq .plugins.available_on_server.oidc
 
 # setup konga and wait some time to be sure that all services are up and running
 docker-compose up -d konga
-sleep 2m
+sleep 5m
 
 # Test liveness for Konga
-curl -s http://localhost:1337
+curl -v http://localhost:1337
 
 # setup keycloak
 docker-compose up -d keycloak-db
@@ -31,7 +31,7 @@ docker-compose up -d keycloak
 docker-compose ps
 
 # Test liveness for Keycloak
-curl -s http://localhost:8180
+curl -v http://localhost:8180
 
 # setup prometheus and grafana
 docker-compose up -d prometheus
@@ -39,7 +39,7 @@ docker-compose up -d grafana
 docker-compose ps
 
 # Test liveness for Prometheus
-curl -s http://localhost:9090
+curl -v http://localhost:9090
 
 # Test liveness for Grafana
-curl -s http://localhost:3000
+curl -v http://localhost:3000
