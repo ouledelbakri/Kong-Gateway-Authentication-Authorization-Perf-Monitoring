9a66c0b88b4354877880601e5d52a854a8d69b53
Yassine OULED EL BAKRI <youledelbakri@PC-3015.gestform.fonctionnel>
2024-09-02 12:01:45 +0200

    Setup CI/CD

--- exec.sh
+++ exec.sh
@@ -5,30 +5,28 @@ set -e
 
 # setup kong
 docker-compose build kong
-sleep 5
+sleep 2
 docker-compose up -d kong-db
-sleep 5
+sleep 2
 docker-compose run --rm kong kong migrations bootstrap
-sleep 5
+sleep 2
 docker-compose run --rm kong kong migrations up
-sleep 5
+sleep 2
 docker-compose up -d kong
 docker-compose ps
+curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
 
-# Test liveness for Kong
-curl -v http://localhost:8001 | jq .plugins.available_on_server.oidc
-
-# setup konga and wait some time to be sure that all services are up and running
+# setup konga and wait some time X to be sure that all services are up and running
 docker-compose up -d konga
-sleep 5m
+sleep 2m
 
-# Test liveness for Konga
-curl -v http://localhost:1337
-
-# setup keycloak
+#setup keycloak
 docker-compose up -d keycloak-db
 docker-compose up -d keycloak
 docker-compose ps
+
+# Test liveness for Konga
+curl -v http://localhost:1337
 
 # Test liveness for Keycloak
 curl -v http://localhost:8180
