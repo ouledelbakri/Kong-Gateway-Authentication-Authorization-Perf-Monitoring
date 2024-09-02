65fdcc276d4ec36d596eda1e869c4e1c712e9daa
Yassine OULED EL BAKRI <youledelbakri@PC-3015.gestform.fonctionnel>
2024-09-02 11:20:57 +0200

    Ajout de Tests de Liveness et de Readiness

--- exec.sh
+++ exec.sh
@@ -5,7 +5,6 @@ set -e
 
 # setup kong
 docker-compose build kong
-
 sleep 2
 docker-compose up -d kong-db
 sleep 2
@@ -15,13 +14,32 @@ docker-compose run --rm kong kong migrations up
 sleep 2
 docker-compose up -d kong
 docker-compose ps
+
+# Test liveness for Kong
 curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
 
-# setup konga and wait some time X to be sure that all services are up and running
+# setup konga and wait some time to be sure that all services are up and running
 docker-compose up -d konga
 sleep 2m
 
-#setup keycloak
+# Test liveness for Konga
+curl -s http://localhost:1337
+
+# setup keycloak
 docker-compose up -d keycloak-db
 docker-compose up -d keycloak
 docker-compose ps
+
+# Test liveness for Keycloak
+curl -s http://localhost:8180
+
+# setup prometheus and grafana
+docker-compose up -d prometheus
+docker-compose up -d grafana
+docker-compose ps
+
+# Test liveness for Prometheus
+curl -s http://localhost:9090
+
+# Test liveness for Grafana
+curl -s http://localhost:3000
